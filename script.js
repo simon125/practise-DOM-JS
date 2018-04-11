7b88063990a2c3e02b385da8cf1fb8bc30e606d2
Szymon Oleszek <szymqwerty@gmail.com>
2018-04-11 10:00:20 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+var firstQuestion =function(){
+
+
+    if(confirm('want you answer the question?')){
+
+        var name = prompt('what is your name')
+
+       alert('hi ' + name)
+    }
+    else alert('ok fine so good bye')
+
+}
+
+
+firstQuestion()
